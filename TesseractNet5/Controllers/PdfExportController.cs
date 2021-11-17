0417b6ed1985d8bbb6af59323566d21150979cfe
Ngọc Châu Ngô <vt_chaunn@itsol.vn>
2021-11-17 17:05:25 +0700

    First Init

---
+++ TesseractNet5/Controllers/PdfExportController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using Tesseract;
+
+namespace TesseractNet5
+{
+    [ApiController]
+    [Route("api/")]
+    public class PdfExportController : ControllerBase
+    {
+        private readonly ILogger<PdfExportController> _logger;
+        private readonly IWebHostEnvironment _hostingEnvironment;
+
+        public PdfExportController(ILogger<PdfExportController> logger, IWebHostEnvironment hostingEnvironment)
+        {
+            _logger = logger;
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        [HttpGet("/getNowDate")]
+        public string GetOrders()
+        {
+            return DateTime.Now.ToString();
+        }
+
+        [HttpPost]
+        [Route("get-text-from-image")]
+        public string GetTextFromPdf(IFormFile file)
+        {
+            //location to testdata for eng.traineddata
+            var path = _hostingEnvironment.WebRootPath + "/AppFile/TrainedData";
+            //your sample image location
+            var text = string.Empty;
+
+            byte[] fileBytes = null;
+            if (file.Length > 0)
+            {
+                using (var ms = new MemoryStream())
+                {
+                    file.CopyTo(ms);
+                    fileBytes = ms.ToArray();
+                }
+            }
+
+            using (var engine = new TesseractEngine(path, "vie"))
+            {
+                using (var img = Pix.LoadFromMemory(fileBytes))
+                {
+                    using (var page = engine.Process(img))
+                    {
+                        text = page.GetText();
+                    }
+                }
+            }
+            return text;
+        }
+    }
+}
---
+++ TesseractNet5/Startup.cs
@@ -0,0 +1,74 @@
+﻿using TesseractNet5.Api.Infrastructure;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace TesseractNet5
+{
+    public class Startup
+    {
+        private const string DefaultCorsPolicyName = "MyPolicy";
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // enable cors 
+            services.AddCors(options =>
+            {
+                options.AddPolicy(DefaultCorsPolicyName,
+                    builder =>
+                    {
+                        builder.WithOrigins(Configuration["App:CorsOrigins"])
+                                            .AllowAnyHeader()
+                                            .AllowAnyMethod();
+                    });
+            });
+
+            services.AddRazorPages();
+            services.AddControllers();
+
+            // add swagger
+            services.AddSwaggerGen(c =>
+            {
+                c.OperationFilter<SwaggerDefaultValues>();
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();              
+            }
+
+            //show ui
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.DisplayOperationId();               
+                c.SwaggerEndpoint($"/swagger/v1/swagger.json", $"Aspose Net 5");
+            });
+
+            app.UseRouting();
+
+            app.UseCors(DefaultCorsPolicyName);
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
